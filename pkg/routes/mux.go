724e01c576d3d57b3267f1af7dae1376b485a3a3
Tony Yip <tony@opensource.hk>
2018-05-08 03:18:17 +0800

    Initial Commit

---
+++ pkg/routes/mux.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"net/http"
+)
+
+type Mux struct {
+	Frontend http.Handler
+
+	AccountProxy *AccountProxy `inject:""`
+}
+
+func (m *Mux) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	if m.Frontend == nil {
+		m.initFrontend()
+	}
+
+	m.Frontend.ServeHTTP(w, r)
+}
+
+func (m *Mux) initFrontend() {
+	mux := http.NewServeMux()
+	mux.Handle("/account", http.StripPrefix("/account", m.AccountProxy))
+
+	m.Frontend = mux
+}
